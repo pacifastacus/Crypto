0b89322c73c9e4f8a50ee40c4976034c2c0498c8
pacifastacus <palkovics.denes@gmail.com>
2019-10-28 18:53:06 +0100

    Command line application is implemented
    -Decrypt mode implemented
    -Output format fixed
    -Multiline textfile handling fixed

--- src/crypto/Program.java
+++ src/crypto/Program.java
@@ -1,11 +1,13 @@
 package crypto;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.Console;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -23,7 +25,8 @@ public final class Program {
 	private static final String CMD_ENC = "encrypt";
 	private static final String CMD_DEC = "decrypt";
 	private static final String CMD_LOAD= "load";
-	private static final String CMD_QUIT = "quit";
+	private static final String CMD_QUIT= "quit";
+	private static final String CMD_BACK= "back";
 	private RSA rsa;
 	private RSAKey key;
 	private Console console;
@@ -64,10 +67,8 @@ public final class Program {
 			out_pk.writeObject(keys.getPublic());
 			out_sk.writeObject(keys.getPrivate());
 		} catch (FileNotFoundException e) {
-			System.err.println("HIBA: nem nyitható meg írásra a kulcsfájlok valamelyike!");
-			e.printStackTrace();
+			System.err.println("HIBA: nem nyitható meg írásra a kulcsfájlok valamelyike! " + e);
 		} catch (IOException e) {
-			System.err.println("HIBA: kimeneti hiba!");
 			e.printStackTrace();
 		}
 	}
@@ -82,28 +83,33 @@ public final class Program {
 				new ObjectInputStream(new FileInputStream(filename))){
 			key = (RSAKey)in_key.readObject();
 		} catch (FileNotFoundException e) {
-			System.err.println("HIBA: A fájl nem található!");
-			e.printStackTrace();
+			System.err.println("HIBA: A fájl nem található! " + e);
 		} catch (ClassNotFoundException e) {
-			System.err.println("HIBA: A fájl nem tartalmaz kulcsot!");
-			e.printStackTrace();
+			System.err.println("HIBA: A fájl nem tartalmaz kulcsot! " + e);
 		} catch (ClassCastException e) {
-			System.err.println("HIBA: A fájlban nem RSA kulcs van!");
-			e.printStackTrace();
+			System.err.println("HIBA: A fájlban nem RSA kulcs van! " + e);
 		} catch (IOException e) {
-			System.err.println("HIBA: bemeneti hiba!");
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * 
+	 */
 	private void encMode() {
+		System.out.println("-------Titkosító üzemmód-------\n");
+		String cmd;
 		EncLoop:
 		while(true) {
-			System.out.println("-------Titkosító üzemmód-------\n\n"
+			System.out.println("\n-------------------\n"
 					+ CMD_LOAD+ " - kulcs betöltése fájlból\n"
 					+ CMD_ENC + " - Szöveges fájl titkosítása\n"
-					+ CMD_QUIT +" - visszalépés\n");
-			switch(console.readLine()) {
+					+ CMD_BACK+" - visszalépés\n");
+			cmd = console.readLine(">");
+			if(cmd == null) {
+				return;
+			}
+			switch(cmd) {
 			case CMD_LOAD:
 				String keyfname = "RSA_PK.key"; 
 				String read = console.readLine("Adja meg a publikus kulcs fájlnevét (%s):",keyfname);
@@ -111,34 +117,97 @@ public final class Program {
 				loadKey(keyfname);
 				break;
 			case CMD_ENC:
-				//Ellenőrizzük, hogy publikus rsa kulcs lett-e beolvasva;
-				System.out.println("kulcs typusa:"+key.getClass());
+				//Ellenőrizzük, hogy van-e kulcs betöltve és az publikus rsa kulcs-e
+				if(key == null) {
+					System.out.println("Nincs nyílvános kulcs betöltve");
+					break;
+				}
+				if(!(key instanceof RSA_PK)) {
+					System.out.println("A betöltött kulcs nem RSA nyílvános kulcs");
+					break;
+				}
 				
-				String textfname = console.readLine("Adja meg a titkosítandó szöveges fájlt");
+				String textfname = console.readLine("Adja meg a titkosítandó szöveges fájlt:");
 				try(BufferedReader infile =
 						new BufferedReader(new FileReader(textfname));
 					ObjectOutputStream outfile = 
-						new ObjectOutputStream(new FileOutputStream(textfname + ".crypt"))) {
-					outfile.writeObject(rsa.encrypt(infile.readLine(), (PublicKey)key));
+						new ObjectOutputStream(new FileOutputStream(textfname.split("\\.")[0] + ".crypt"))) {
+					StringBuffer strBuff = new StringBuffer();
+					String line = null;
+					while((line =infile.readLine())!=null){
+						strBuff.append(line).append("\n");
+					}
+					outfile.writeObject(rsa.encrypt(strBuff.toString(), (PublicKey)key));
+					System.out.println(textfname + " fájl titkosítva!\n");
 				} catch (FileNotFoundException e) {
-					System.err.println("HIBA: a fájl nem található!");
-					e.printStackTrace();
+					System.err.println("HIBA: a fájl nem található!" + e);
 				} catch (IOException e) {
 					e.printStackTrace();
 				} catch (InvalidKeyException e) {
-					System.err.println("HIBA: A betöltött kulcs nem rsa kulcs");
-					e.printStackTrace();
+					System.err.println("HIBA: A betöltött kulcs nem rsa kulcs "+ e);
 				}
-				System.out.println(textfname + " fájl titkosítva!");
 				break;
-			case CMD_QUIT:	break EncLoop;
+			case CMD_BACK:	break EncLoop;
+			case CMD_QUIT: break EncLoop;
 			default: System.out.println("Ismeretlen utasítás!"); continue EncLoop;
 			}
 		}
-		
-		
-		
 	}
+		
+	private void decMode() {
+		System.out.println("-------Visszafejtő üzemmód-------\n");
+		DecLoop:
+		while(true) {
+			System.out.println("\n-------------------\n"
+					+ CMD_LOAD+ " - kulcs betöltése fájlból\n"
+					+ CMD_DEC + " - titkosított fájl visszafejtése\n"
+					+ CMD_BACK +" - visszalépés\n");
+			switch(console.readLine()) {
+			case CMD_LOAD:
+				String keyfname = "RSA_SK.key"; 
+				String read = console.readLine("Adja meg a titkos kulcs fájlnevét (%s):",keyfname);
+				if(!read.isBlank()) keyfname = read;
+				loadKey(keyfname);
+				break;
+			case CMD_DEC:
+				//Ellenőrizzük, hogy van-e kulcs betöltve és az publikus rsa kulcs-e
+				if(key == null) {
+					System.out.println("Nincs nyílvános kulcs betöltve");
+					break;
+				}
+				if(!(key instanceof RSA_SK)) {
+					System.out.println("A betöltött kulcs nem RSA titkos kulcs");
+					break;
+				}
+				
+				String codefname = console.readLine("Adja meg a titkosítandó szöveges fájlt:");
+				try(BufferedWriter outfile =
+						new BufferedWriter(new FileWriter(codefname.split("\\.")[0] + ".txt"));
+					ObjectInputStream infile = 
+						new ObjectInputStream(new FileInputStream(codefname))) {
+					
+					Object readObject = infile.readObject();
+					if(!(readObject instanceof Queue<?>))
+						throw new ClassNotFoundException("readObject is not a Queue");
+					outfile.write(rsa.decrypt((Queue<BigInteger>)readObject, (PrivateKey)key));
+					System.out.println(codefname + " fájl visszafejtve!\n");
+				} catch (FileNotFoundException e) {
+					System.err.println("HIBA: a fájl nem található! "+e);
+				} catch (InvalidKeyException e) {
+					System.err.println("HIBA: A betöltött kulcs nem rsa kulcs! "+e);
+				} catch (ClassNotFoundException e) {
+					System.err.println("HIBA: A beolvasott fájl nem szabványos! "+e);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				break;
+			case CMD_BACK:	break DecLoop;
+			case CMD_QUIT: break DecLoop;
+			default: System.out.println("Ismeretlen utasítás!"); continue DecLoop;
+			}
+		}
+	}
+		
 	/**
 	 * Runs the main application
 	 * @param args - the commandline arguments not yet implemented
@@ -146,6 +215,7 @@ public final class Program {
 	public static void main(String[] args) {
 		Program prg = new Program();
 		prg.printGreeting();
+		String cmd;
 		MainLoop:
 		while(true) {
 			System.out.println("A továbblépéshez gépelje be az alábbiak valamelyikét:\n\n"
@@ -153,10 +223,15 @@ public final class Program {
 					+ CMD_ENC +" - titkosító üzemmód.\n"
 					+ CMD_DEC +" - visszafejtő üzemmód\n"
 					+ CMD_QUIT+" - kilépés a programból\n");
-			switch (prg.console.readLine()) {
+			
+			cmd = prg.console.readLine(">");
+			if(cmd == null) {
+				return;
+			}
+			switch (cmd) {
 			case CMD_GEN:	prg.generateKeys(); break; 
 			case CMD_ENC:	prg.encMode(); break;
-			case CMD_DEC:	break;
+			case CMD_DEC:	prg.decMode(); break;
 			case CMD_QUIT:	break MainLoop;
 			default: System.out.println("Ismeretlen utasítás!"); continue MainLoop;
 			}
